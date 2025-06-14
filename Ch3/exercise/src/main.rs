6a06215385a0bec56e1b80e7854ea36f4b7db9ef
Jingkun Liu <jingkunliu2025@u.northwestern.edu>
2025-06-14 19:24:12 -0400

    add ch1

--- Ch3/exercise/src/main.rs
+++ Ch3/exercise/src/main.rs
@@ -1,3 +1,6 @@
+use std::thread;
+use std::vec;
+
 fn main() {
     println!("Hello, world!");
 
@@ -16,14 +19,21 @@ fn fahrenheit_to_celsius(deg_f: f32) -> f32 {
     (deg_f - 32.0) * 5.0 / 9.0
 }
 
-fn fibonacci(n: u32) -> u32 {
-    if n == 0 {
+fn fibonacci(n: u32) -> u128 {
+    if n <= 1 {
         return 1;
     }
 
-    if n == 1 {
-        return 1;
+    let mut fib_vec = vec![1; n as usize];
+
+    for i in 2..n {
+        let fib_i1 = fib_vec[(i - 1) as usize];
+        let fib_i2 = fib_vec[(i - 2) as usize];
+
+        let fib_i = fib_i1 + fib_i2;
+
+        fib_vec[i as usize] = fib_i;
     }
 
-    return fibonacci(n - 1) + fibonacci(n - 2);
+    return fib_vec[(n - 1) as usize];
 }
